7a02bea87e17e6d86102e77e5d2b58e5c973969a
jcare282 <jacob.carey@marriott.com>
2014-05-30 16:56:06 -0400

    Remove all old files

--- auto_panda/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from auto_panda.models import UserProfile
-
-admin.site.register(UserProfile)
--- auto_panda/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from auto_panda import views
-
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^register/$', views.register, name='register'),
-	url(r'^logout/$', views.user_logout, name='logout'),
-	url(r'^upload/$', views.upload, name='upload'),
-)
